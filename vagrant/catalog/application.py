2f0a17ee84640ee0ed5e02aad6160eb554454273
Andrew Downie <andrew_dev@outlook.com>
2017-09-19 15:52:38 -0400

    Skeletals done for view / manage items. Going to add modals for the user to modify/add/delete items.

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -1,15 +1,38 @@
 from flask import Flask
 from flask import render_template 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route("/")
-def hello():
-    meow_meow = [{"meow_name": "al"}, {"meow_name": "jess"}]
-    return render_template('index.html', body_content="<h1>fart</h1>", meows=meow_meow)
+@app.route("/index.html")
+def view_items():
+
+    mock_categories = [
+      {"category_name": "Tools"},
+      {"category_name": "Food"},
+      {"category_name": "Kitchen"}
+    ]
 
 
-#return app.send_static_file('index.html')
-#return "Hello World!"
+    mock_items = [
+      {"item_name": "Hammer",   "category_name": "Tools"},
+      {"item_name": "Apple",   "category_name": "Food"},
+      {"item_name": "Banana",   "category_name": "Food"},
+      {"item_name": "Pear",   "category_name": "Food"},
+      {"item_name": "Blender", "category_name": "Kitchen"}
+    ]
+
+
+    return render_template('index.html', categories=mock_categories, items=mock_items)
+
+@app.route("/manage.html")
+def manage_items():
+
+    mock_items = [
+      {"item_name": "Taco", "category_name": "Food", "id": "abcid", "desc": "This... this is a taco, what else do you really really really really need to know?"}
+    ]
+
+    return render_template('manage.html', items=mock_items)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8000)
