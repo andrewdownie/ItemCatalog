57499e7d9cc175a35d73e0550fd6f829fafff6a2
Andrew Downie <andrew_dev@outlook.com>
2017-09-19 12:03:09 -0400

    Figured out how to template with flask, now to actually create the html pages needed for this project.

---
+++ vagrant/catalog/application.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import render_template 
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template('index.html', body_content="fart")
+
+
+#return app.send_static_file('index.html')
+#return "Hello World!"
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=8000)
